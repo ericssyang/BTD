da0a11d9ffe419fc7cead3a52377b68fe4fa4c07
Su San Yang <ericssyang@gmail.com>
2016-09-15 13:24:51 -0400

    Create lab1.sql

---
+++ lab1.sql
@@ -0,0 +1,10 @@
+/* Question 1 */
+select employee_id, last_name
+from employees
+where salary > 5000 and job_id = 'IT_PROG';
+/* Question 2 */
+
+/* Question 3 */
+/* Question 4 */
+/* Question 5 */
+/* Question 6 */
