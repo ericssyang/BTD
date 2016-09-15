ac404059877d8cec4a3296e5657a700ba3ad2e4e
Su San Yang <ericssyang@gmail.com>
2016-09-15 13:29:05 -0400

    Update lab1.sql

--- lab1.sql
+++ lab1.sql
@@ -3,8 +3,14 @@ select employee_id, last_name
 from employees
 where salary > 5000 and job_id = 'IT_PROG';
 /* Question 2 */
-
+select last_name || ', ' || first_name || ', ' || job_id
+from employees
+order by job_id desc;
 /* Question 3 */
+select last_name, salary
+from employees
+order by salary desc;
 /* Question 4 */
+
 /* Question 5 */
 /* Question 6 */
